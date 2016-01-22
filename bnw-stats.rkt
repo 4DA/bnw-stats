cce3f478ba2796e987ae82d5714b6290922bf299
Dmitry Cherkassov <dcherkassov@gmail.com>
2016-01-22 03:02:47 +0300

    parallelize http connections

--- bnw-stats.rkt
+++ bnw-stats.rkt
@@ -1,16 +1,18 @@
 #lang racket
+
 (require racket/match)
 (require net/http-client)
 (require json)
+
 
 (define *bnw-host-name* "bnw.im")
 (define *stats-filename* "/home/dc/bnwstats.dot")
 (define *msgcount-criteria* 10)
 (define *comments-criteria* 20)
 (define *lastmsg-criteria* 15768000) ;; half year should be enough
-(define *default-fontsize* 20)
+(define *default-fontsize* 30)
 (define *max-font-factor* (* *default-fontsize* 2))
-
+(define *threads-num* 20)
 
 (struct global-ctx (userhash))
 (struct userinfo (name subscribers subscriptions messages-count
@@ -33,8 +35,8 @@
   (when (hash-ref jsl 'ok)
     (define uh (hash-ref jsl 'users))
     (if (eq? uh '()) '()        
-      (for/list ([entry (in-list uh)])
-        (hash-ref entry 'name)))))
+        (for/list ([entry (in-list uh)])
+          (hash-ref entry 'name)))))
 
 (define/contract (get-all-usernames ctx)
   (-> global-ctx? list?)
@@ -52,6 +54,10 @@
       (hash-ref (last messages)
                 'date)))
 
+(define/contract (user-known? ctx name)
+  (-> global-ctx? string? boolean?)
+  (hash-has-key? (global-ctx-userhash ctx) name))
+
 (define/contract (user-actually-exists? ctx name)
   (-> global-ctx? string? boolean?)
   (define jsl (make-request ctx (format "/api/userinfo?user=~a" name)))
@@ -62,16 +68,21 @@
   (define jsl (make-request ctx (format "/api/userinfo?user=~a" name)))
 
   (if (eq? #t (hash-ref jsl 'ok))
-    (userinfo name
-              (hash-ref jsl 'subscribers_all)
-              (hash-ref jsl 'subscriptions_all)
-              (hash-ref jsl 'messages_count)
-              (hash-ref jsl 'regdate)
-              (hash-ref jsl 'comments_count)
-              (get-user-lastmessage-time ctx name))
-  (error (format "Can not request user info: ~a\nResponse: ~a" name jsl))))
+      (userinfo name
+                (hash-ref jsl 'subscribers_all)
+                (hash-ref jsl 'subscriptions_all)
+                (hash-ref jsl 'messages_count)
+                (hash-ref jsl 'regdate)
+                (hash-ref jsl 'comments_count)
+                (get-user-lastmessage-time ctx name))
+      (error (format "Can not request user info: ~a\nResponse: ~a" name jsl))))
 
 (define/contract (get-user-info ctx name)
+  (-> global-ctx? string? userinfo?)
+  (define userhash (global-ctx-userhash ctx))
+  (hash-ref userhash name))
+
+(define/contract (get-user-info/memoize ctx name)
   (-> global-ctx? string? userinfo?)
 
   (define userhash (global-ctx-userhash ctx))
@@ -92,18 +103,17 @@
   (-> global-ctx? string? void?)
 
   (define alice-uinfo (get-user-info ctx alice))
-  (if (user-meets-criteria? alice-uinfo)
-      (match-let 
-          ([(userinfo name subscribers subscriptions messages-count regdate comments-count lastmessage-time)
-            alice-uinfo])
+  (when (user-meets-criteria? alice-uinfo)
+    (match-let 
+        ([(userinfo name subscribers subscriptions messages-count regdate comments-count lastmessage-time)
+          alice-uinfo])
 
-        (eprintf "~a: building reader graph\n" alice)
-        (for ([bob (in-list subscribers)])
-          (when (and (user-actually-exists? ctx bob)
-                     (user-meets-criteria? (get-user-info ctx bob)))
-            (printf "\"~a\" -> \"~a\"\n" bob alice))))
-
-      (eprintf "~a: doesn't meet criteria\n" alice)))
+      (eprintf "~a: building reader graph\n" alice)
+      (for ([bob (in-list subscribers)])
+        (when (and #t
+                   (user-known? ctx bob)
+                   (user-meets-criteria? (get-user-info ctx bob)))
+          (printf "\"~a\" -> \"~a\"\n" bob alice))))))
 
 (define/contract (output-nodes-info ctx)
   (-> global-ctx? void?)
@@ -135,16 +145,45 @@
         sc
         max-subs)))
 
-(define (generate-dot-file ctx)
+(define/contract (split-list-to-n lst n)
+  (-> list? integer? list?)
+  
+  (define slice-sz (floor (/ (length lst) n)))
+  (define (do-split remlst resn)
+    (if (eq? resn 1)
+        (list remlst)
+        (append (list (take remlst slice-sz))
+                (do-split (drop remlst slice-sz) (sub1 resn)))))
+
+  (do-split lst n))
+
+(define/contract (init-userhash ctx)
+  (-> global-ctx? any)
+  (eprintf "Retrieving user info.\n")
+  (define userlist (get-all-usernames ctx))
+  (define ul-len (length userlist))
+
+  (define worksize (floor (/ ul-len *threads-num*)))
+
+  (define thread-list
+    (for/list ([workset (in-list (split-list-to-n userlist *threads-num*))])
+      (thread
+       (lambda ()
+         (for ([u (in-list workset)])
+           (get-user-info/memoize ctx u))))))
+
+  (for ([tr (in-list thread-list)])
+    (thread-wait tr)))
+
+(define/contract (generate-dot-file ctx)
+  (-> global-ctx? void?)
   (with-output-to-file *stats-filename*
     (λ () (output-dot ctx))
     #:mode 'text
     #:exists 'truncate))
 
-;;(output-dot-for-user *bnw-ctx* "ninesigns")
-;;(output-nodes-info *bnw-ctx*)
-(generate-dot-file *bnw-ctx*)
-
-
-
+(time
+ (init-userhash *bnw-ctx*))
+(time
+ (generate-dot-file *bnw-ctx*))
 
